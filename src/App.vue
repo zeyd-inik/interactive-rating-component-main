ee0ee2db4fb94414c5b2152f264efe47d8af4075
zeyd <1zeydinik@gmail/com>
2023-01-15 16:09:07 +0100

    update app.vue styles

--- src/App.vue
+++ src/App.vue
@@ -4,4 +4,19 @@
     <RouterView />
 </template>
 
-<style></style>
+<style>
+.title {
+    font-size: 1.5rem;
+    font-weight: 700;
+    color: white;
+    margin: 1rem 0;
+}
+.text {
+    font-size: 0.9rem;
+    font-weight: 400;
+    color: var(--white);
+    line-height: 1.5;
+    transform: translateY(-20px);
+    margin-top: 1rem;
+}
+</style>
